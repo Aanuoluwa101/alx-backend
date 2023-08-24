b516b8c7f058e72d8077771f494a469c5559b321
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-24 14:53:23 +0000

    type checking  for 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,8 +40,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0 \
-            and isinstance(page_size, int) and page_size > 0
+        assert type(page == int) and page > 0 \
+            and type(page_size == int) and page_size > 0
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
 
