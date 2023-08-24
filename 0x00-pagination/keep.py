e3194f40c7751eda013937b0e29f6d4b6b849569
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-24 13:24:32 +0000

    fixed 0 again

---
+++ 0x00-pagination/keep.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+"""
+Defines a function that returns a tuple of size two
+containing a start index and an end index
+corresponding to the range of indexes to return
+in a list for some particular pagination parameters.
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Returns a tuple of size two
+    containing a start index and an end index
+    """
+    # start_index = page_size * (page - 1)
+    # end_index = start_index + page_size
+    # return start_index, end_index
+    return ((page-1) * page_size, page_size * page)
