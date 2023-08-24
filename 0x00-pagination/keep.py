36159473ba2b69d02f45f17cdd70d25a70673dca
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-24 15:12:21 +0000

    fixed 1

--- 0x00-pagination/keep.py
+++ 0x00-pagination/keep.py
@@ -1,20 +1,51 @@
 #!/usr/bin/env python3
 
 
-"""
-Defines a function that returns a tuple of size two
-containing a start index and an end index
-corresponding to the range of indexes to return
-in a list for some particular pagination parameters.
-"""
+"""Defines a function that returns the
+   appropriate page of a dataset"""
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of size two
     containing a start index and an end index
     """
-    # start_index = page_size * (page - 1)
-    # end_index = start_index + page_size
-    # return start_index, end_index
-    return ((page-1) * page_size, page_size * page)
+    start_index = page_size * (page - 1)
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) is int and page > 0 \
+            and type(page_size) is int and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        try:
+            return dataset[start_index:end_index]
+        except IndexError:
+            return []
