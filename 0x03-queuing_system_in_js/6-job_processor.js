e7378f5cfe6fa78c0c25a707654e9fa892a698d5
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-09-27 21:17:13 -0700

    queuing systems in js

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,11 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+function sendNotification(phoneNumber, msg) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${msg}`);
+}
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+})
